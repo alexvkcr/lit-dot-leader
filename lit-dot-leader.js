ba5b383253caa6999b6b1d302bf8e9756bad5600
Diaz Muñoz <alexvkcr@gmail.com>
2019-10-10 18:05:50 +0200

    fix: deletion of TypeScript

--- lit-dot-leader.js
+++ lit-dot-leader.js
@@ -1,23 +1,15 @@
-import { LitElement, html, customElement, property } from 'lit-element';
-/**
- * Use the customElement decorator to define your class as
- * a custom element. Registers <my-element> as an HTML tag.
- */
-@customElement('lit-dot-leader')
-export class LitDotLeader extends LitElement {
-    /**
-     * Create an observed property. Triggers update on change.
-     */
-    @property()
-    foo = 'foo';
-    /**
-     * Implement `render` to define a template for your element.
-     */
-    render() {
-        /**
-         * Use JavaScript expressions to include property values in
-         * the element template.
-         */
-        return html`<p>${this.foo}</p>`;
-    }
+// Import the LitElement base class and html helper function
+import { LitElement, html } from 'lit-element';
+
+// Extend the LitElement base class
+class LitDotLeader extends LitElement {
+
+  render(){
+    return html`
+      <!-- template content -->
+      <p>A paragraph</p>
+    `;
+  }
 }
+// Register the new element with the browser.
+customElements.define('lit-dot-leader', LitDotLeader);
