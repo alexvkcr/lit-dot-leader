cfedc02d6b1a2e4a7b5618df94bcfc784ea34741
Alejandro Díaz <alexvkcr@gmail.com>
2019-10-13 13:11:35 +0200

    feat: properties initialized and shown

--- lit-dot-leader.js
+++ lit-dot-leader.js
@@ -1,13 +1,24 @@
-// Import the LitElement base class and html helper function
 import { LitElement, html } from 'lit-element';
 
-// Extend the LitElement base class
 class LitDotLeader extends LitElement {
+  static get properties() { return {
+    lines: { type: Array },/*This property stores an array that has an entry for each line that you want to have 'dot-leadered'
+                          Every entry will have two values, the text, and the price*/
+    separator: {type: String }/*This sets up what will be the character to type in separate the text and price*/
+  }}
+
+  constructor() {
+    super();
+    this.lines = [{text:'Loren Ipsum',
+                  price:'99€'}];
+    this.separator='.'
+  }
 
   render(){
     return html`
-      <!-- template content -->
-      <p>A paragraph</p>
+      <span>${this.lines[0]['text']}</span>
+      ${this.separator.repeat(3)}
+      <span>${this.lines[0]['price']}</span>
     `;
   }
 }
